2ebb5049db9ed0e5a571b051d1118b9ef0f0a15e
Eduarda Machado <95512620+du17@users.noreply.github.com>
2025-07-01 15:18:51 -0300

    Update conexao.php

--- adocao_animais/conexao.php
+++ adocao_animais/conexao.php
@@ -4,9 +4,10 @@ $host = 'localhost';
 $dbname = 'bd_adocao';
 $username = 'root';
 $password = '';
+$port = '3307';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
